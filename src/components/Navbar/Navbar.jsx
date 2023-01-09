4236a3b2cbdc271da17c65a05018e5cb301c39a3
Tamilore <tamiloreolaniyi@gmail.com>
2023-01-09 13:55:50 +0100

    mobile-nav-complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,6 +16,17 @@ function Navbar() {
             <li><a href="/" className='cta'>View Plans</a></li>
         </ul>
         {active ? <img src='https://res.cloudinary.com/dnovlrekd/image/upload/v1671690027/icon-close_mzowtd.svg' alt='' onClick={() => setActive(!active)}/> : <img src='https://res.cloudinary.com/dnovlrekd/image/upload/v1671690028/icon-hamburger_v2jes6.svg' alt='' onClick={() => setActive(!active)}/> }
+        {active && 
+        <div className="mobile-nav">
+            <ul className="mobile-nav-links">
+            <li><a href="/">How we work</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Account</a></li>
+            <li><a href="/" className='cta'>View Plans</a></li>
+        </ul>
+        <img src="https://res.cloudinary.com/dnovlrekd/image/upload/v1671690027/bg-pattern-mobile-nav_miho9t.svg" alt="" className='bg-mobile-nav' />
+        </div>
+        }
     </div>
   )
 }
